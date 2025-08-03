a1d4be6202193744ba7b48e093f40ef0f05ea599
timmy16744 <timhughes92@gmail.com>
2025-08-04 02:44:57 +0930

    Fix: Restore htmlTemplateEngine setting in .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,7 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/index.html");
 
   return {
+    htmlTemplateEngine: false,
     dir: {
       input: "src",
       output: "dist"
